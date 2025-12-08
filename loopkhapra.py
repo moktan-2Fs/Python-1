240fadccb6daabbea936859ba87c76d31ca78482
Moktan Sagar <moktansagar761@gmail.com>
2025-12-08 08:23:50 +0545

    doing if and elif in py and unders.. some other basic concepts

--- loopkhapra.py
+++ loopkhapra.py
@@ -21,7 +21,7 @@
 # while i <= len(my_list)-1:
 #   print(my_list[i], i)
 
-#   i += 1 
+#   i += 1
 # i = 1
 # my_string = ["moktan","rohan","sohan","mohan"]
 # while i < len(my_string):
@@ -29,24 +29,68 @@
 #   i += 2
 
 # my_list = (1,3,3,4,5,3,6454,4534,34,34,342,2,32,64,433)
-# i = 0 
+# i = 0
 # x = 34
 # while i < len(my_list):
 #   if my_list[i] == x:
 #     print(i , my_list[i])
 #   i += 1
 
-# i = 1 
+# i = 1
 # while i <= 5:
 #   print(i)
 #   if (i == 4):
 #     break
 #   i += 1
-   
-# i = 0 
-# while i<=5: #this is an example of while loop in python 
-#   if(i==3): 
+
+# i = 0
+# while i<=5: #this is an example of while loop in python
+#   if(i==3):
 #     i += 1
 #     continue
 #   print(i)
-#   i += 1 
+#   i += 1
+
+# import math
+# radius = float(input("Enter the radius of the circle: "))
+# area = 2*math.pi*pow(radius, 2)
+# print(f"The area of the circle is: {area}")
+
+# response = input("Whold you go out on a date with me?(Y/y|N/n)").strip()
+# if response == "N" or response == "n":
+#     print("It's fine. It was good to know you.")
+# elif response == "Y" or response == "y":
+#     print("Great lets do that next Saturday..")
+# else:
+#     print("Its good to know that and yeah its ok..")
+# if response == "":
+#     print("you didnt type anything")
+# else:
+#     print("its alright..")
+
+# for_sale = False
+# if for_sale:
+#     print("You are for sale..")
+# else:
+#     print("Not for sale...")
+
+#calculator in python
+# oper = input("Enter any operator to perform the task: ")
+# num1 = float(input("Enter 1st number: "))
+# num2 = float(input("Enter 2nd number: "))
+# result = None
+# if oper == "":
+#     print("Enter a valid ")
+# if oper == "+":
+#     result = num1 + num2 
+# elif oper == "-":
+#     result = num1 - num2 
+# elif oper == "/":
+#     result = num1 / num2
+# elif oper == "%":
+#     result = num1 % num2
+# elif oper == "*":
+#     result = num1 * num2
+# else:
+#     print("Please enter a valid operator...")
+# print("The final result is: ", result)
