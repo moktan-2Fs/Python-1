90e5c17b9c2c4ee7fbfff7333699e83b7ab99ddc
Sagar Tamang <sagarmoktan2369@gmail.com>
2025-12-16 19:11:43 +0545

    from computer commit

--- functionskhapra.py
+++ functionskhapra.py
@@ -5,7 +5,7 @@
 # print(calc_sum(num1,num2))
 # print(calc_sum(5,8)+calc_sum(num1,num2))
 
-def cal_ave(a=1,b=1,c=1):
-  return (a+b+c)/3
-print(cal_ave(5,3))
+# def cal_ave(a=1,b=1,c=1):
+#   return (a+b+c)/3
+# print(cal_ave(5,3))
 
