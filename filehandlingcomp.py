b9c51ee756b1f86db7506b0f9e2b4fcdedd45080
Sagar Tamang <sagarmoktan2369@gmail.com>
2025-12-17 21:21:10 +0545

    deleting some unignored file to create a new ignored file

--- filehandlingcomp.py
+++ filehandlingcomp.py
@@ -9,3 +9,14 @@
 # f = open("compfile.txt","r")
 # for li in f:
 #   print(f.readline())
+
+# f = open("compfile.txt","r+")
+# f.write("Writing in r+ mode;....")
+# print(f.read())
+# # f.write("\nNew line added to the file from the filehandlingcomp.py file")
+# print(f.read())
+# f.close()
+
+with open("moktan.txt","w+") as file:
+  file.write("Hello form the other side.....")
+  
