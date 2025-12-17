407c77e6e49400196d2c06811003e5ddf4d7b05c
Sagar Tamang <sagarmoktan2369@gmail.com>
2025-12-17 20:23:13 +0545

    random commitss

---
+++ filehandlingcomp.py
@@ -0,0 +1,11 @@
+# f =  open("compfile.txt","w")
+# f.write("hello from the other side....\n")
+# f.write("This is also a file that I wrote from the .write() function.")
+
+# f = open("compfile.txt","r")
+# print(f.readline(),end= "")
+# print(f.read(6))
+
+# f = open("compfile.txt","r")
+# for li in f:
+#   print(f.readline())
