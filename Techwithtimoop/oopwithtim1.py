7f1bc0aebc87a40a9aad3af66e0a00db8d29037e
Sagar Tamang <sagarmoktan2369@gmail.com>
2025-12-23 07:37:14 +0545

    trying new tutorial for OOP in python

---
+++ Techwithtimoop/oopwithtim1.py
@@ -0,0 +1,16 @@
+# def hello():
+#     print("Gello")
+
+# print(type(hello))
+# print(type(1))
+# print(type("hello"))
+# print(type(33.43))
+
+class Dog:
+    def __init__(self):
+        pass
+    def bark(self):
+        print("Bark!!!!!")
+do = Dog()
+do.bark()
+print(type(do))
---
+++ WebDevPython/day_q.py
@@ -0,0 +1,19 @@
+from flask import Flask, request, jsonify
+
+app = Flask(__name__)
+
+@app.route("/get-user/<user_id>")
+def get_user(user_id):
+
+    
+if __name__ == "__main__":
+    app.run(debug=True)
+
+#http is the way to communicate over the internet
+#mathods to work in apis 
+# GET  used to retrieve data from a server , request data from a specified resurece
+# POST  create something new, create a resourse
+# PUT  used to alter or modifi existing datas, update a resourec
+# DELETE used to remove ro delete data from the data source 
+
+# path parameter dinamic value that can be passed inthe path of the url 
