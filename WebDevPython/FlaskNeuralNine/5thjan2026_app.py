04f124d6b63d3ac6c733928608d1c80191f8b338
Sagar Tamang <sagarmoktan2369@gmail.com>
2026-01-05 14:50:02 +0545

    mkingappto add numbers in flask

--- WebDevPython/FlaskNeuralNine/5thjan2026_app.py
+++ WebDevPython/FlaskNeuralNine/5thjan2026_app.py
@@ -6,5 +6,20 @@ app = Flask(__name__)
 def index():
   return '<h1 style="background-color: red;">hello moktan</h1>'
 
+@app.route("/endpoint")
+# /endpoint is used to invoke this function in the website 
+# its like going to a different webpage after enterign /endpoint in the url
+def hello():
+  return "Hello Sagor Tomang!!!!"
+
+@app.route("/greet/<name>")
+def greet(name):
+  return f"hello {name}"
+
+@app.route("/add/<int:num1>/<int:num2>") #declarign the type of numbers
+def add(num1,num2): 
+  return f"The sum of {num1} & {num2} is: {num1+num2}"
+
+
 if __name__ == '__main__':
   app.run(host= "0.0.0.0",debug= True)
